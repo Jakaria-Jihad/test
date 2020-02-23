799f15ee0861e72d20574e674d903ca6537f7c3e
Jakaria-Jihad <61376128+Jakaria-Jihad@users.noreply.github.com>
2020-02-23 14:42:19 +0500

    Update prog1.c

--- prog1.c
+++ prog1.c
@@ -4,3 +4,20 @@ int main()
     int choice;
     printf("Enter a number:");
     scanf("%d", &choice);
+    
+    switch(choice)
+    {
+        case 1:
+            printf("You typred %d", choice);
+            break;
+        case 2:
+            printf("You typed %d", choice);
+            break;
+        case 3:
+            printf("You typed %d", choice);
+            break;
+        default:
+            printf("You typed something else");
+    }
+    return 0;
+}
